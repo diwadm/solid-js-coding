360effcda87f89c33b399a5f918d285936ae4527
Diwa del Mundo <diwadm@gmail.com>
2015-05-24 18:36:02 +0800

    Updated #4 - #5.

--- javascript-review/5.hoisting.js
+++ javascript-review/5.hoisting.js
@@ -1,3 +1,12 @@
-/**
- * Created by diwadelmundo on 5/24/15.
- */
+b = 'bee';
+
+function func() {
+  console.log(b);
+
+  // 'b' will get hoisted, i.e. interpreter declares before execution.
+  var b = 'local bee';
+
+  console.log(b);
+}
+
+func();
