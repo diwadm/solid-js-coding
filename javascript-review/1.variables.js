2cef234795acf7deed07d354a67f6738221736c1
Diwa del Mundo <diwadm@gmail.com>
2015-05-24 16:10:07 +0800

    Added #1 and #2 examples.

---
+++ javascript-review/1.variables.js
@@ -0,0 +1,13 @@
+// Primitives
+
+var s = 'string';
+
+var num = 123;
+
+var bool = true;
+
+// This will result to 'object' due to historical reasons.
+// Objects' type is 0 and null is 0x00, so ambiguous.
+var aNull = null;
+
+var undef = undefined;
---
+++ javascript-review/2.object-literals.js
@@ -0,0 +1,26 @@
+// Object literal
+var o = {};
+// Object constructor
+var oc = new Object();
+
+// Examples of literals and built-in constructors.
+
+// Array literal
+var a = [];
+// Array constructor
+var ac = new Array();
+
+// Regular expression literal
+var r = /javascript/g;
+// Regular expression constructor
+var rc = new RegExp('javascript', 'g');
+
+// String, number, and boolean
+var s = 'dog';
+var sc = new String();
+
+var n = 123;
+var nc = new Number();
+
+var b = false;
+var bc = new Boolean();
